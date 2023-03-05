a896f5af6bc23f165138269926d2560e43ff2510
Lissandro kenziano <lissandromr@hotmail.com>
2023-03-05 19:06:24 -0300

    feat: criando component header e incluindo logo

---
+++ resources/js/components/header/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <header >
+        <div class="logo-container">
+            <img src="../../.././../public/LISTRA_DIGITAL.png" alt="Logo Listra Digital"/>
+        </div>
+    </header>
+</template>
+
+<script>
+
+    export default {
+        data(){
+            
+        } ,
+        methods:{
+           
+        }
+    };
+</script>
+
+<style scoped>
+.logo-container {
+  padding:15px;
+  text-align: center;
+  background-color: #7D28F7;
+}
+
+</style>
